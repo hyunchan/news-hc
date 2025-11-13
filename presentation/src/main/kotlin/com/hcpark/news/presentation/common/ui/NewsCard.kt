54d4328bb63c12c0c6ad833deac8080e0ff4a16e
hcpark <hcpark@adenasoft.com>
2025-11-13 13:56:14 +0900

    feat: 뉴스 이미지 로딩 실패 시 아이콘 표시

--- presentation/src/main/kotlin/com/hcpark/news/presentation/common/ui/NewsCard.kt
+++ presentation/src/main/kotlin/com/hcpark/news/presentation/common/ui/NewsCard.kt
@@ -2,16 +2,19 @@ package com.hcpark.news.presentation.common.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.BookmarkBorder
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.ImageNotSupported
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -23,6 +26,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -54,14 +58,29 @@ fun NewsCard(
     ) {
         Column {
             // Hero Image
-            AsyncImage(
-                model = model.imageUrl,
-                contentDescription = null,
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(200.dp),
-                contentScale = ContentScale.Crop
-            )
+                contentAlignment = Alignment.Center
+            ) {
+                var error by remember { mutableStateOf(false) }
+
+                AsyncImage(
+                    model = model.imageUrl,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    onLoading = { error = false },
+                    onError = { error = true }
+                )
+                if (error)
+                    Icon(
+                        modifier = Modifier.size(52.dp),
+                        imageVector = Icons.Outlined.ImageNotSupported,
+                        contentDescription = "Image Not Supported",
+                        tint = colorScheme.secondary,
+                    )
+            }
 
             Column(modifier = Modifier.padding(16.dp)) {
                 // Source Badge
@@ -112,18 +131,18 @@ fun NewsCard(
                     IconButton(onClick = onBookmark) {
                         Icon(
                             imageVector =
-                            if (model.isBookmarked) {
-                                Icons.Filled.Bookmark
-                            } else {
-                                Icons.Default.BookmarkBorder
-                            },
+                                if (model.isBookmarked) {
+                                    Icons.Filled.Bookmark
+                                } else {
+                                    Icons.Default.BookmarkBorder
+                                },
                             contentDescription = "Bookmark",
                             tint =
-                            if (model.isBookmarked) {
-                                colorScheme.primary
-                            } else {
-                                Color.Unspecified
-                            }
+                                if (model.isBookmarked) {
+                                    colorScheme.primary
+                                } else {
+                                    Color.Unspecified
+                                }
                         )
                     }
                 }
